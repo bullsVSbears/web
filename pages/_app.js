8023192a237ad43999d55cc0f773a96c5e7494e6
Deedayo <waxptabitha@gmail.com>
2021-08-06 15:33:00 +0100

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
-
+import 'tailwindcss/tailwind.css'
+import '../styles/style2.css'
+// import '../styles/normalize.css'
+// import '../styles/webflow.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
